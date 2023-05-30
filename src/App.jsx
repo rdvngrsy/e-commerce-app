f10dda20baad03d323fe56a249b26dbfb0c662b4
Ridvan Gursoy <ridvangursoy@hotmail.com>
2023-05-30 19:35:26 +0300

    Dosyalar oluşturuldu.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from "./components/Header";
+
+
+const App = () => {
+  
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <h1>RIDVAN</h1>
+    </>
+  )
+}
+
+export default Header;
